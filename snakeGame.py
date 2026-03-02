de7fa352e293d6af1236e9cf1b893fb5d1c6ff87
Ahmad34-90 <ahmiiikha@gmail.com>
2026-03-02 22:49:48 +0500

    creating the x-direction motion

--- snakeGame.py
+++ snakeGame.py
@@ -11,6 +11,9 @@ screen_height = 600
 snake_x =45
 snake_y= 55
 snake_size =10
+fps = 30             # fps = FramePerSecond
+
+clock = pygame.time.Clock()
 
 # Creating Window
 gameWindow = pygame.display.set_mode((screen_width, screen_height))
@@ -27,10 +30,15 @@ while not exit_game:
     for event in pygame.event.get():  
         print(event)
         if event.type == pygame.QUIT:
-            exit_game = True    
+            exit_game = True   
+
+        if event.type == pygame.KEYDOWN:          # Right moment
+            if event.key == pygame.K_RIGHT:
+                snake_x = snake_x + 10 
 
     gameWindow.fill(white)
-    pygame.draw.rect(gameWindow, black, [snake_x, snake_y, snake_size, snake_size]) #Snake character Creation
+    pygame.draw.rect(gameWindow, black, [snake_x, snake_y, snake_size, snake_size])               #Snake character Creation
+    clock.tick(fps)
     pygame.display.update()
 pygame.quit() 
 exit()
