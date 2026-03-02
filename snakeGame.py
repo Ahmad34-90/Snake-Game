704cf5cc69a19aad1206407753337034299fa831
Ahmad34-90 <ahmiiikha@gmail.com>
2026-03-02 22:39:06 +0500

    Snake Character Creation

--- snakeGame.py
+++ snakeGame.py
@@ -3,9 +3,15 @@ pygame.init()
 
 #Colors
 white = (255, 255, 255, 1)
+black = (0, 0, 0 , 0)
 
+#Game Specific Variable
 screen_width =1200
 screen_height = 600
+snake_x =45
+snake_y= 55
+snake_size =10
+
 # Creating Window
 gameWindow = pygame.display.set_mode((screen_width, screen_height))
 
@@ -18,12 +24,13 @@ game_over = False
 
 #Game Loop
 while not exit_game:
-    for event in pygame.event.get():
+    for event in pygame.event.get():  
         print(event)
         if event.type == pygame.QUIT:
             exit_game = True    
 
     gameWindow.fill(white)
+    pygame.draw.rect(gameWindow, black, [snake_x, snake_y, snake_size, snake_size]) #Snake character Creation
     pygame.display.update()
 pygame.quit() 
 exit()
