a26b0479acec1add36477f658eeca8804c73e011
Ahmad34-90 <ahmiiikha@gmail.com>
2026-03-02 21:23:31 +0500

    Handling / learning  the key!

--- flappyBird.py
+++ flappyBird.py
@@ -8,10 +8,17 @@ pygame.display.set_caption("Flappy Bird Game!")   #Game Title.
 exit_game = False
 game_over = False
 
-while not exit_game: #game loop.
-    for event in pygame.event.get():  #handling events
-        if event.type == pygame.quit():
+#Game loop....
+while not exit_game: 
+    for event in pygame.event.get():        #handling events...
+        if event.type == pygame.QUIT:
             exit_game = True
+
+        if event.type ==pygame.KEYDOWN:
+            if event.key ==pygame.K_RIGHT:
+                print("You have pressed right key of KeyBoard.")
+            if event.key == pygame.K_UP:
+                print("You have pressed Up key.")
 
 pygame.quit()
 exit()
