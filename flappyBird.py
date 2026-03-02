0cf34aa4e1ead8d28825a6de8e5950fe63ff5bf6
Ahmad34-90 <ahmiiikha@gmail.com>
2026-03-02 21:13:44 +0500

    Adding exit event

--- flappyBird.py
+++ flappyBird.py
@@ -1,6 +1,17 @@
 import pygame
-x = pygame.init()
+pygame.init()
 
-gameWindow = pygame.display.set_mode((1200,500))   #dispaly setup
+gameWindow = pygame.display.set_mode((1200,500))   #Display setup.
 
-pygame.display.set_caption("Flappy Bird Game!")
+pygame.display.set_caption("Flappy Bird Game!")   #Game Title.
+
+exit_game = False
+game_over = False
+
+while not exit_game: #game loop.
+    for event in pygame.event.get():  #handling events
+        if event.type == pygame.quit():
+            exit_game = True
+
+pygame.quit()
+exit()
